c39da0f592b5a166b96a7c16b9d7e26c7b93f7b9
Zachjrivera <Zachjrivera@gmail.com>
2018-11-29 11:04:47 -0500

    finished

---
+++ src/main/java/com/zach/pokemonedatabase/PokemonedatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.zach.pokemonedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonedatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonedatabaseApplication.class, args);
+	}
+}
